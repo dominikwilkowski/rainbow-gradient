59adffd6208acab71aa6736b0fd969e0d58fdd99
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2019-09-29 15:10:55 +1000

    fixed degree / radiant conversion
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- gradient.js
+++ gradient.js
@@ -153,7 +153,7 @@ function hex2rgb( hex ) {
 function hex2xyz( hex ) {
 	const rgb = hex2rgb( hex );
 	const [ h, x, z ] = rgb2hsv( rgb );
-	const y = Math.PI * h;
+	const y = h * 360 * ( Math.PI / 180 ); // convert to radiant
 
 	return [ x, y, z ];
 }
@@ -168,8 +168,8 @@ function hex2xyz( hex ) {
  * @return {string}    - A HEX color
  */
 function xyz2hex( x, y, z ) {
-	const degree = Math.round( Math.PI / y );
-	const [ r, g, b ] = hsv2rgb( degree, x, z );
+	const h = y / (360 * ( Math.PI / 180 )); // convert to degree
+	const [ r, g, b ] = hsv2rgb( h, x, z );
 
 	return rgb2hex( r, g, b );
 }
@@ -296,10 +296,11 @@ function Gradient( fromColor, toColor, steps ) {
 	const [ fromCoordX, fromCoordY, fromCoordZ ] = hex2xyz( fromColor );
 	const [ toCoordX, toCoordY, toCoordZ ] = hex2xyz( toColor );
 
-	console.log(`${ fromColor } -> ${ fromCoordX }, ${ fromCoordY }, ${ fromCoordZ } -> ${ xyz2hex( fromCoordX, fromCoordY, fromCoordZ ) }`);
+	// console.log(`${ fromColor } -> ${ fromCoordX }, ${ fromCoordY }, ${ fromCoordZ } -> ${ xyz2hex( fromCoordX, fromCoordY, fromCoordZ ) }`);
+	// console.log(`${ toColor } -> ${ toCoordX }, ${ toCoordY }, ${ toCoordZ } -> ${ xyz2hex( toCoordX, toCoordY, toCoordZ ) }`);
 
-	// console.log(`fromX: ${ fromCoordX } fromY: ${ fromCoordY } fromZ: ${ fromCoordZ }`);
-	// console.log(`toX: ${ toCoordX } toY: ${ toCoordY } toZ: ${ toCoordZ }`);
+	console.log(`fromX: ${ fromCoordX } fromY: ${ fromCoordY } fromZ: ${ fromCoordZ }`);
+	console.log(`toX: ${ toCoordX } toY: ${ toCoordY } toZ: ${ toCoordZ }`);
 
 	const [ fromRadius, fromTheta, fromZ ] = coord2cylindrical( fromCoordX, fromCoordY, fromCoordZ );
 	const [ toRadius, toTheta, toZ ] = coord2cylindrical( toCoordX, toCoordY, toCoordZ );
@@ -315,9 +316,10 @@ function Gradient( fromColor, toColor, steps ) {
 		const theta = getTheta( fromTheta, toTheta, n, ( steps - 1 ) );
 		const z = getLinear( fromZ, toZ, n, ( steps - 1 ) );
 
-		const [ x, y ] = cylindrical2coord( radius, theta );
+		// const [ x, y ] = cylindrical2coord( radius, theta );
 
-		console.log(`radius: ${ radius } theta: ${ theta } z: ${ z } hex: ${ xyz2hex( x, y, z ) }`);
+		// console.log(`radius: ${ radius } theta: ${ theta } z: ${ z } hex: ${ xyz2hex( radius, theta, z ) }`);
+		console.log(`<div style="background: ${ xyz2hex( radius, theta, z ) };width:5rem;height:5rem"></div>`);
 	}
 }
 
